44f2d9f1a4f231914b3e651288716bd703b7eb57
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-06-27 21:35:37 +0200

    added new package practice

---
+++ src/practice/Task1.java
@@ -0,0 +1,53 @@
+package practice;
+
+import javax.swing.JOptionPane;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+
+		int Row = Integer.parseInt(JOptionPane.showInputDialog("Input number of rows: "));
+		int Column = Integer.parseInt(JOptionPane.showInputDialog("Input number of columns: "));
+
+		int[][] spiral = new int[Row][Column];
+
+		int value = 1;
+		int r1 = 0;
+		int r2 = Row - 1;
+		int c1 = 0;
+		int c2 = Column - 1;
+
+		while (value <= Row * Column) {
+			// from first row left to right
+			for (int i = c1; i <= c2; i++) {
+				spiral[r1][i] = value++;
+			}
+			// from r1+1 down to r2-1
+
+			for (int j = r1 + 1; j <= r2; j++) {
+				spiral[j][c2] = value++;
+			}
+			// from c2-1 right to c1 left
+			for (int i = c2 - 1; i >= c1; i--) {
+				spiral[r2][i] = value++;
+			}
+			// from r2-1 down up to r1+1
+			for (int j = r2 - 1; j >= r1 + 1; j--) {
+				spiral[j][c1] = value++;
+			}
+			c1++;
+			r1++;
+			c2--;
+			r2--;
+
+		}
+
+		for (int i = 0; i < Row; i++) {
+			for (int j = 0; j < Column; j++) {
+				System.out.print(spiral[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
--- src/table/Task.java
+++ src/table/Task.java
@@ -1,70 +1,62 @@
 package table;
 
-
-
 import javax.swing.JOptionPane;
 
 public class Task {
-	
+
 	public static void main(String[] args) {
-		
-		int Rows=Integer.parseInt(JOptionPane.showInputDialog("a"));
-		int Columns=Integer.parseInt(JOptionPane.showInputDialog("b"));
-		
-		int spiral [][]= new int[Rows][Columns];
-		
-		int value =1;
-		int minRow=0;
-		int maxRow=Rows-1;
-		int minColumn=0;
-		int maxColumn=Columns-1;
-		
-		
-		
-		while(value <= Columns*Rows) {
-			//from final row  right bottom to left bottom
-		for(int i=maxColumn;i>=minColumn;i--) {
-			spiral [maxRow][i]=value++;
-		
-			
+
+		int Row = Integer.parseInt(JOptionPane.showInputDialog("Input number of Rows: "));
+		int Column = Integer.parseInt(JOptionPane.showInputDialog("Input number of Columns: "));
+
+		int spiral[][] = new int[Row][Column];
+
+		int value = 1;
+		int r1 = 0;
+		int r2 = Row - 1;
+		int c1 = 0;
+		int c2 = Column - 1;
+
+		while (value <= Column * Row) {
+			// from final row right bottom to left bottom
+			for (int i = c2; i >= c1; i--) {
+				spiral[r2][i] = value++;
+
+			}
+			// from final row left bottom up to first row left top
+			for (int i = r2 - 1; i >= r1; i--) {
+				spiral[i][c1] = value++;
+
+			}
+			// from first row left top to right top
+			for (int i = c1 + 1; i <= c2; i++) {
+				spiral[r1][i] = value++;
+
+			}
+
+			// from right max row-3 to right max row-1
+
+			for (int i = r1 + 1; i <= r2 - 1; i++) {
+				spiral[i][c2] = value++;
+
+			}
+
+			c1++;
+
+			r1++;
+
+			c2--;
+
+			r2--;
 		}
-		//from final row left bottom up to first row left top
-		for(int i=maxRow-1;i>=minRow;i--) {
-			spiral[i][minColumn]=value++;
-			
-		}
-		//from first row left top to right top
-		for(int i=minColumn +1;i<=maxColumn;i++) {
-			spiral[minRow][i]=value++;
-		
-		}
-		
-		//from right max row-3 to right max row-1
-		
-		for(int i=minRow +1;i<=maxRow-1;i++) {
-			spiral[i][maxRow]=value++;
-			
-		}
-		
-		
-		minColumn++;
-        
-        minRow++;
-         
-        maxColumn--;
-         
-        maxRow--;
-		}
-		
-		for(int i=0;i<Rows;i++) {
-			for(int j=0;j<Columns;j++) {
-				System.out.print(spiral[i][j]+ "\t");
+
+		for (int i = 0; i < Row; i++) {
+			for (int j = 0; j < Column; j++) {
+				System.out.print(spiral[i][j] + "\t");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
-
 }
-
